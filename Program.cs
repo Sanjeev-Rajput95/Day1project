b0eb27b373f2fef725846823487ccdc679315093
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-11 22:31:26 +0530

    Take Input from User

--- Program.cs
+++ Program.cs
@@ -10,7 +10,14 @@ namespace SanjeevDay1project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Type your username and press enter
+            Console.WriteLine("Enter username:");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
+
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is: " + userName);
         }
     }
 }

c06921f3c180eea50fe3fb9dafae1e3e7501e7da
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-11 14:48:37 +0530

    Print Hello-World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day1project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

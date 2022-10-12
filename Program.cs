962f49bc2329efce7f158e8bf09cfe5c8c3764c1
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-12 08:24:37 +0530

    UPDATED : Taking User FirstName and LastName

--- Program.cs
+++ Program.cs
@@ -11,13 +11,16 @@ namespace SanjeevDay1project
         static void Main(string[] args)
         {
             // Type your username and press enter
-            Console.WriteLine("Enter username:");
+            Console.WriteLine("Enter first name: ");
+            Console.WriteLine("Enter last name : ");
 
             // Create a string variable and get user input from the keyboard and store it in the variable
-            string userName = Console.ReadLine();
+            string FirstName = Console.ReadLine();
+            string LastName = Console.ReadLine();
 
             // Print the value of the variable (userName), which will display the input value
-            Console.WriteLine("Username is: " + userName);
+            Console.WriteLine("Username is: " + FirstName);
+            Console.WriteLine("Username is: " + LastName);
         }
     }
 }
